4a72cc167f7b6506245f0cb483e95acd52654aeb
pragmaticreviews <info@pragmaticreviews.com>
2022-01-19 21:53:56 -0300

    CI with AWS CodePipeline

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const text = process.env.TEXT || 'Hello World From NestJS!';    
+    return text;
   }
 }
